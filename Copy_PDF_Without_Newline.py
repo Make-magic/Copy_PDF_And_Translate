fe6305e0727b0f3aedb7a6f991a87a4aef8e5fa8
upcxksorce <34813726+upcxksorce@users.noreply.github.com>
2020-08-17 12:43:30 +0800

    Update Copy_PDF_Without_Newline.py

--- Copy_PDF_Without_Newline.py
+++ Copy_PDF_Without_Newline.py
@@ -2,8 +2,9 @@ import pyperclip
 import time
 print('准备就绪，请复制')
 while True:
-    time.sleep(2)
+    time.sleep(0.2)
     copyedText = pyperclip.paste()
     if True:
         changedText = copyedText.replace("\r", "\\r").replace("\n", "\\n").replace("-\\r\\n", "").replace("\\r\\n", " ")
+        time.sleep(0.2)
         pyperclip.copy(changedText)
