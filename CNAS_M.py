a5a176379f2ba1e0cb1e785d27574e9e731250d8
upcxksorce <34813726+upcxksorce@users.noreply.github.com>
2021-08-10 20:07:46 +0800

    Create CNAS_M.py

---
+++ CNAS_M.py
@@ -0,0 +1,35 @@
+import pyautogui
+import pyperclip
+import time
+
+class AC_saladict():
+    def __init__(self):
+        self.size = pyautogui.size()
+        self.pos = pyautogui.position()
+
+    def copy(self):
+        global tem_text
+        global text
+        if self.pos[0] < 10:
+            pyautogui.hotkey('ctrl','c')
+            text = pyperclip.paste().replace("\r", "\\r").replace("\n", "\\n").replace("-\\r\\n", "").replace("\\r\\n", " ")
+            if tem_text != text:
+                tem_text = text
+                self.convey_saladict()
+        elif self.pos[0] > self.size[0] - 10:
+            pyautogui.hotkey('ctrl','c')
+            exit()
+            
+    def convey_saladict(self):
+        pyperclip.copy(tem_text)
+        pyautogui.hotkey('alt','l')
+            
+
+if __name__ == '__main__':
+    tem_text = ''
+    text = ''
+    print('准备就绪，请复制')
+    pyautogui.FAILSAFE = True
+    while True:
+        time.sleep(0.1)
+        AC_saladict().copy()
